8643982c88047278cd85bf67e2c445e3a3836a34
kubotake-desktop <keigo1003560@gmail.com>
2025-09-15 14:38:35 +0900

    fix: 不要なテストファイルを削除

--- jest/workspace/frontend/src/__tests__/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('renders Jest Practice heading', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Jest Practice - Frontend/i);
-  expect(linkElement).toBeInTheDocument();
-});
